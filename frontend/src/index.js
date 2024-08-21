81ec57363e564ce0966b82ecdc78b55c16d62a46
B.C.R.N.P.BANDARAADHIKARAM <92729642+CharithaRuwindu@users.noreply.github.com>
2024-08-21 19:55:00 +0530

    initialize backend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,20 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from './pages/Layout';
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Blogs from "./pages/Blogs";
 
 export default function App() {
-  return(
+  return (
     <div>
       <BrowserRouter>
-      <Routes>
-        <Route path='/' element={Layout}>
-
-        </Route>
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="blogs" element={< Blogs/>}/>
+          </Route>
+        </Routes>
       </BrowserRouter>
     </div>
   );
-};
-const root = ReactDOM.createRoot(document.getElementById('root'));
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
---
+++ frontend/src/pages/Blogs.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Blogs = () => {
+    return (
+        <h1>Hello this is the blog</h1>
+    );
+
+};
+
+export default Blogs;
