c009e752efcaea14331db9cdf2651d0653f81b6a
B.C.R.N.P.BANDARAADHIKARAM <92729642+CharithaRuwindu@users.noreply.github.com>
2024-08-27 23:56:26 +0530

    Update Layout.jsx

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -4,7 +4,7 @@ const Layout = () => {
   return (
     <>
       <nav className="bg-yellow-600 h-14 content-center">
-        <ul className="flex bg-red-400">
+        <ul className="flex bg-red-400 place-content-end">
           <li>
             <Link to="/">Home</Link>
           </li>
@@ -13,6 +13,9 @@ const Layout = () => {
           </li>
           <li>
             <Link to="/contact">Contact</Link>
+          </li>
+          <li>
+            <Link to="/menu">Menu</Link>
           </li>
         </ul>
       </nav>
