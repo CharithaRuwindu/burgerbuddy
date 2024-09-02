fbca8635202c6f84d476edf5bf32df15d9f4133e
B.C.R.N.P.BANDARAADHIKARAM <92729642+CharithaRuwindu@users.noreply.github.com>
2024-09-02 23:25:06 +0530

    redoing backend

---
+++ backend/backend/Models/Menu.cs
@@ -0,0 +1,11 @@
+﻿namespace backend.Models
+{
+    public class Menu
+    {
+        public int Menu_ID { get; set; }
+        public string? Name { get; set; }
+        public string? Category { get; set; }
+        public double Price { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
