cfc2a21651960b4c15b2543743b2eb47d3535726
B.C.R.N.P.BANDARAADHIKARAM <92729642+CharithaRuwindu@users.noreply.github.com>
2024-09-11 21:43:21 +0530

    starting the component

--- frontend/src/components/ItemCard.jsx
+++ frontend/src/components/ItemCard.jsx
@@ -1,11 +1,10 @@
-import React from "react";
-
-const itemcard = () => {
-
-return (
-    <>
-    
-    </>
-);
-};
-export default itemcard;
+export default function ItemCard({ itemName, itemPrice, itemAvailability }) {
+    return (
+      <div>
+        <h2>{itemName}</h2>
+        <h3>{itemPrice}</h3>
+        <p>{itemAvailability}</p>
+      </div>
+    );
+  }
+  
--- frontend/src/pages/Blogs.jsx
+++ frontend/src/pages/Blogs.jsx
@@ -1,37 +1,40 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import ItemCard from "../components/ItemCard";
 
 const Blogs = () => {
-
     const [menus, setMenus] = useState(null);
 
     useEffect(() => {
         const fetchData = async () => {
-            try{
+            try {
                 const response = await axios.get(`/api/Menus`);
                 setMenus(response.data);
-            }catch(error)
-            {
+            } catch (error) {
                 console.log("Error fetching data:", error);
             }
         };
         fetchData();
     }, []);
-    
+
     return (
         <>
-        <h1 className="font-bold">Hello this is the blog</h1>
+            <h1 className="font-bold">Hello, this is the blog</h1>
 
-        {menus ? (
-                <div>hi</div>
+            {menus ? (
+                menus.map((menu, index) => (
+                    <ItemCard
+                        key={menu.menu_ID}
+                        itemName={menu.name}
+                        itemPrice={`LKR ${menu.price}`}
+                        itemAvailability={menu.isAvailable ? "Available" : "Unavailable"}
+                    />
+                ))
             ) : (
-                <div>Loading</div>
-            )
-        }
+                <div>Loading...</div>
+            )}
         </>
-
     );
-
 };
 
 export default Blogs;
