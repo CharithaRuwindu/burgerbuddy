c2fb84f71e81e5e767f482ac0a0b255f64f826a9
B.C.R.N.P.BANDARAADHIKARAM <92729642+CharithaRuwindu@users.noreply.github.com>
2024-09-17 23:47:25 +0530

    menu selector updating

--- frontend/src/components/MenuSelector.jsx
+++ frontend/src/components/MenuSelector.jsx
@@ -1,6 +1,29 @@
+import { AiOutlineCoffee } from "react-icons/ai";
+import { CiBurger } from "react-icons/ci";
+import { CiFries } from "react-icons/ci";
+import { BiBowlRice } from "react-icons/bi";
+import { PiBowlFood } from "react-icons/pi";
+
 export default function MenuSelector() {
     return (
         <>
+        <div>
+            <div><CiBurger />
+            <p>Burgers</p>
+            </div>
+            <div><BiBowlRice />
+            <p>Meals</p>
+            </div>
+            <div><CiFries />
+            <p>Sides</p>
+            </div>
+            <div><AiOutlineCoffee />
+            <p>Drinks</p>
+            </div>
+            <div><PiBowlFood />
+            <p>Desserts</p>
+            </div>
+        </div>
         </>
     )
 }
