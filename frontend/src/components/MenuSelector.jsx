bfa15d8e76e3c193afc4792be38a75ebe4e7c1d5
B.C.R.N.P.BANDARAADHIKARAM <92729642+CharithaRuwindu@users.noreply.github.com>
2024-09-18 23:27:23 +0530

    state management of menu buttons

--- frontend/src/components/MenuSelector.jsx
+++ frontend/src/components/MenuSelector.jsx
@@ -3,25 +3,38 @@ import { CiBurger } from "react-icons/ci";
 import { CiFries } from "react-icons/ci";
 import { BiBowlRice } from "react-icons/bi";
 import { PiBowlFood } from "react-icons/pi";
+import { useState, useEffect } from "react";
+
 
 export default function MenuSelector() {
+    const[Active, setActive] = useState(1);
+
+    function ChangeSelection (index){
+        console.log(index);
+    }
+
     return (
         <>
-        <div>
-            <div><CiBurger />
-            <p>Burgers</p>
+        <div className="text-white">
+            <div className="bg-red-400 rounded-full w-16 h-16 flex shadow-2xl" onClick={() => ChangeSelection(1)}>
+                <span className="m-auto"><CiBurger className="m-auto w-8 h-8"/>
+            <p className="text-xs">Burgers</p></span>
             </div>
-            <div><BiBowlRice />
-            <p>Meals</p>
+            <div className="bg-red-400 rounded-full w-16 h-16 flex shadow-2xl" onClick={() => ChangeSelection(2)}>
+            <span className="m-auto"><BiBowlRice className="m-auto w-8 h-8"/>
+            <p className="text-xs">Meals</p></span>
             </div>
-            <div><CiFries />
-            <p>Sides</p>
+            <div className="bg-red-400 rounded-full w-16 h-16 flex shadow-2xl" onClick={() => ChangeSelection(3)}>
+            <span className="m-auto"><CiFries className="m-auto w-8 h-8"/>
+            <p className="text-xs">Sides</p></span>
             </div>
-            <div><AiOutlineCoffee />
-            <p>Drinks</p>
+            <div className="bg-red-400 rounded-full w-16 h-16 flex shadow-2xl" onClick={() => ChangeSelection(4)}>
+            <span className="m-auto"><AiOutlineCoffee className="m-auto w-8 h-8"/>
+            <p className="text-xs">Drinks</p></span>
             </div>
-            <div><PiBowlFood />
-            <p>Desserts</p>
+            <div className="bg-red-400 rounded-full w-16 h-16 flex shadow-2xl" onClick={() => ChangeSelection(5)}>
+            <span className="m-auto"><PiBowlFood className="m-auto w-8 h-8"/>
+            <p className="text-xs">Desserts</p></span>
             </div>
         </div>
         </>
