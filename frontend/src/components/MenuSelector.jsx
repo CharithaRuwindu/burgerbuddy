4d3fb102636c3823da014dcab1c288a5901458f2
B.C.R.N.P.BANDARAADHIKARAM <92729642+CharithaRuwindu@users.noreply.github.com>
2024-09-19 22:06:18 +0530

    states to button click

--- frontend/src/components/MenuSelector.jsx
+++ frontend/src/components/MenuSelector.jsx
@@ -10,29 +10,34 @@ export default function MenuSelector() {
     const[Active, setActive] = useState(1);
 
     function ChangeSelection (index){
-        console.log(index);
+        setActive(index);
+    }
+
+    function getMenuItemClass(index) {
+        return Active === index ? "bg-green-400 rounded-full w-20 h-20 flex shadow-2xl cursor-pointer"
+        : "bg-red-400 rounded-full w-16 h-16 flex shadow-2xl cursor-pointer"; // Change to green when active
     }
 
     return (
         <>
         <div className="text-white">
-            <div className="bg-red-400 rounded-full w-16 h-16 flex shadow-2xl" onClick={() => ChangeSelection(1)}>
+            <div className={`${getMenuItemClass(1)}`} onClick={() => ChangeSelection(1)}>
                 <span className="m-auto"><CiBurger className="m-auto w-8 h-8"/>
             <p className="text-xs">Burgers</p></span>
             </div>
-            <div className="bg-red-400 rounded-full w-16 h-16 flex shadow-2xl" onClick={() => ChangeSelection(2)}>
+            <div className={`${getMenuItemClass(2)}`} onClick={() => ChangeSelection(2)}>
             <span className="m-auto"><BiBowlRice className="m-auto w-8 h-8"/>
             <p className="text-xs">Meals</p></span>
             </div>
-            <div className="bg-red-400 rounded-full w-16 h-16 flex shadow-2xl" onClick={() => ChangeSelection(3)}>
+            <div className={`${getMenuItemClass(3)}`} onClick={() => ChangeSelection(3)}>
             <span className="m-auto"><CiFries className="m-auto w-8 h-8"/>
             <p className="text-xs">Sides</p></span>
             </div>
-            <div className="bg-red-400 rounded-full w-16 h-16 flex shadow-2xl" onClick={() => ChangeSelection(4)}>
+            <div className={`${getMenuItemClass(4)}`} onClick={() => ChangeSelection(4)}>
             <span className="m-auto"><AiOutlineCoffee className="m-auto w-8 h-8"/>
             <p className="text-xs">Drinks</p></span>
             </div>
-            <div className="bg-red-400 rounded-full w-16 h-16 flex shadow-2xl" onClick={() => ChangeSelection(5)}>
+            <div className={`${getMenuItemClass(5)}`} onClick={() => ChangeSelection(5)}>
             <span className="m-auto"><PiBowlFood className="m-auto w-8 h-8"/>
             <p className="text-xs">Desserts</p></span>
             </div>
