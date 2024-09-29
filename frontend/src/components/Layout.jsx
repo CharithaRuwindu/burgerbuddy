81ee552017c80aae81620742669b1e0bf6e8acd3
B.C.R.N.P.BANDARAADHIKARAM <92729642+CharithaRuwindu@users.noreply.github.com>
2024-09-29 23:47:55 +0530

    updating login

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -3,19 +3,19 @@ import { Outlet, Link } from "react-router-dom";
 const Layout = () => {
   return (
     <>
-      <nav className="bg-yellow-600 h-14 content-center">
+      <nav className="bg-yellow-600 shadow-md text-stone-100 font-medium h-14 content-center">
         <ul className="flex place-content-end space-x-20 mr-8">
-          <li>
+          <li className="my-auto">
             <Link to="/">Home</Link>
           </li>
-          <li>
+          <li className="my-auto">
             <Link to="/menu">Menu</Link>
           </li>
-          <li>
+          <li className="my-auto">
             <Link to="/contact">Contact</Link>
           </li>
-          <li>
-            <button className="bg-slate-400 w-16 h-8"><Link to="/login">Login</Link></button>
+          <li className="my-auto">
+            <Link to="/login"><button className="py-1 px-4 bg-yellow-800 rounded shadow-md">Login</button></Link>
           </li>
         </ul>
       </nav>
