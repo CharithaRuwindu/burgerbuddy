85c475921860cc7e012623ffe3bc1ccf8e1d2a68
B.C.R.N.P.BANDARAADHIKARAM <92729642+CharithaRuwindu@users.noreply.github.com>
2024-09-30 23:51:41 +0530

    starting login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 
 const Login = () => {
-    return ( 
-        <h1 className="font-bold">This is Login page</h1>
+    return (
+        <div>
+            <div className="rounded shadow-md h-10 w-40">
+                <form action="">
+                    <label htmlFor="">Email
+                        <input type="text" />
+                    </label>
+                    <label htmlFor="">
+                        Password
+                        <input type="text" />
+                    </label>
+                </form>
+            </div>
+        </div>
     );
 };
 
