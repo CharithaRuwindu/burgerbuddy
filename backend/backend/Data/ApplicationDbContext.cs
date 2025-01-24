d139dfe9c2717c677852dbda2fde279ff0a5b591
B.C.R.N.P.BANDARAADHIKARAM <92729642+CharithaRuwindu@users.noreply.github.com>
2025-01-24 23:48:11 +0530

    addin user continue

--- backend/backend/Data/ApplicationDbContext.cs
+++ backend/backend/Data/ApplicationDbContext.cs
@@ -6,24 +6,6 @@ namespace backend.Data
     public class ApplicationDbContext : DbContext
     {
         public DbSet<Menu> Menus { get; set; }
-        //public DbSet<User> Users { get; set; }
-
-        //public DbSet<Role> Roles { get; set; }
-
-        //protected override void OnModelCreating(ModelBuilder modelBuilder)
-        //{
-      //      modelBuilder.Entity<Role>()
-       //         .HasData(
-      //              new Role { RoleId = 1, RoleName = "Admin" },
-      //              new Role { RoleId = 2, RoleName = "Kitchen Staff" },
-      //              new Role { RoleId = 3, RoleName = "Delivery Staff" }
-      //          );
-
-       //     modelBuilder.Entity<User>()
-       //         .HasOne(u => u.Roles)
-       //         .WithMany(r => r.Users)
-       //         .HasForeignKey(u => u.RoleId)
-        //        .OnDelete(DeleteBehavior.Restrict);
-        //}
+        
     }
 }
