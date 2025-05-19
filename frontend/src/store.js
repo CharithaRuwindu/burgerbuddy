8774f65582f96dc84c759bef7d5d7b158a6d17e2
B.C.R.N.P.BANDARAADHIKARAM <92729642+CharithaRuwindu@users.noreply.github.com>
2025-05-20 00:00:03 +0530

    initialize persistor

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,28 @@
+// src/app/store.js
 import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from '../src/reducers/cartSlice';
+import { combineReducers } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import cartReducer from '../features/cart/cartSlice';
 
-export const store = configureStore({
-  reducer: {
-    cart: cartReducer,
-  },
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart']
+};
+
+const rootReducer = combineReducers({
+  cart: cartReducer,
 });
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+export const persistor = persistStore(store);
